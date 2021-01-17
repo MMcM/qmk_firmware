169b1c9fdb3efcd5952f4eb5119fcb8bebe6edc5
Mike McMahon <mmcm@comcast.net>
2021-01-17 09:01:42 -0800

    converter/hp_46010a (adjust for Beetle)

--- keyboards/converter/hp_46010a/rules.mk
+++ keyboards/converter/hp_46010a/rules.mk
@@ -2,7 +2,7 @@
 MCU = atmega32u4
 
 # Bootloader selection
-BOOTLOADER = halfkay
+BOOTLOADER = caterina
 
 # Build Options
 #   change yes to no to disable
@@ -11,7 +11,7 @@ BOOTMAGIC_ENABLE = no       # Enable Bootmagic Lite
 MOUSEKEY_ENABLE = yes       # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
 CONSOLE_ENABLE = yes        # Console for debug
-COMMAND_ENABLE = no         # Commands for debug and configuration
+COMMAND_ENABLE = yes        # Commands for debug and configuration
 NKRO_ENABLE = yes           # Enable N-Key Rollover
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 AUDIO_ENABLE = no           # Audio output
