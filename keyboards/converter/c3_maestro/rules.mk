623263ea5f4e9a01cd0d2ca82d2be913822abbdb
raoulduke-esq <mgmobile@gmail.com>
2021-04-24 10:56:01 -0400

    converter/c3_maestro (squashed)

---
+++ keyboards/converter/c3_maestro/rules.mk
@@ -0,0 +1,17 @@
+# MCU info
+MCU_FAMILY = KINETIS
+MCU_SERIES = K20x
+MCU_LDSCRIPT = MK20DX256
+MCU_STARTUP = k20x7
+BOARD = PJRC_TEENSY_3_1
+MCU = cortex-m4
+ARMV = 7
+BOOTLOADER = halfkay
+
+
+BOOTMAGIC_ENABLE = no       
+MOUSEKEY_ENABLE = yes       
+EXTRAKEY_ENABLE = yes       
+CONSOLE_ENABLE = yes        
+COMMAND_ENABLE = yes             
+NKRO_ENABLE = yes           
