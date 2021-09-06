20cc1aa71ceba568c238959fd0ddaa00087236ee
Mike McMahon <mmcm@comcast.net>
2021-09-06 14:03:18 -0700

    tkc/m0lly:mmcm

--- users/mmcm/mmcm.sh
+++ users/mmcm/mmcm.sh
@@ -63,4 +63,5 @@ make kprepublic/bm16a:mmcm SILENT=true &&
 make kprepublic/bm43a:mmcm SILENT=true &&
 make tkc/tkc1800:mmcm SILENT=true &&
 make converter/oasys SILENT=true &&
+make tkc/m0lly:mmcm SILENT=true &&
 true
