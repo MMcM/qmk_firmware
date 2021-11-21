409aad8a2a754e710234e3585156a1808dbeff65
Mike McMahon <mmcm@comcast.net>
2021-11-20 16:57:45 -0800

    gh60/revc:mmcm_bow

--- users/mmcm/mmcm.sh
+++ users/mmcm/mmcm.sh
@@ -74,4 +74,5 @@ make converter/qume/qvt_61 SILENT=true &&
 make bx_220 SILENT=true &&
 make converter/c3_maestro SILENT=true &&
 make converter/deko_fak SILENT=true &&
+make gh60/revc:mmcm_bow SILENT=true &&
 true
