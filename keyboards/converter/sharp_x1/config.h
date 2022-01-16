7c9ae0f63e48add68eb4e698678c47408cdbdd21
Mike McMahon <mmcm@comcast.net>
2022-01-15 23:47:55 -0800

    converter/sharp_x1

---
+++ keyboards/converter/sharp_x1/config.h
@@ -0,0 +1,10 @@
+
+#pragma once
+
+#include "config_common.h"
+
+#define PRODUCT           Sharp X1 Turbo keyboard converter
+#define DESCRIPTION       Sharp X1 Turbo keyboard converter
+
+#define MATRIX_ROWS       12
+#define MATRIX_COLS       8
