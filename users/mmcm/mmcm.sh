a9c3ad1fe6830ea8e34aab776c1430a604197fbe
Mike McMahon <mmcm@comcast.net>
2022-03-13 08:45:02 -0700

    clueboard/2x1800/2021:mmcm

--- users/mmcm/mmcm.sh
+++ users/mmcm/mmcm.sh
@@ -90,4 +90,5 @@ make converter/memorex_2051 SILENT=true &&
 make keebio/bdn9/rev2:mmcm SILENT=true &&
 make converter/nec_pc_8801 SILENT=true &&
 make converter/ibm_5291 SILENT=true &&
+make clueboard/2x1800/2021:mmcm SILENT=true &&
 true
