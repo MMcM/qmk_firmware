5b59b52dd916a46cb2bd5092f2bf6b749c5b3f96
Mike McMahon <mmcm@comcast.net>
2022-03-20 20:57:09 -0700

    update ymdk

--- users/mmcm/mmcm.sh
+++ users/mmcm/mmcm.sh
@@ -78,5 +78,5 @@ make gh60/revc:mmcm_bow SILENT=true &&
 make converter/uts SILENT=true &&
 make keebio/tukey:mmcm SILENT=true &&
 make navi10:mmcm SILENT=true &&
-make yd60mq:mmcm SILENT=true &&
+make ymdk/yd60mq/16led:mmcm SILENT=true &&
 true
