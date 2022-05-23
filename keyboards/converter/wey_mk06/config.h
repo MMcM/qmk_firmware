99fbfa7157930eba07b8832d6580bdb4b87641ba
Mike McMahon <mmcm@comcast.net>
2022-05-22 20:34:12 -0700

    converter/wey_mk06

---
+++ keyboards/converter/wey_mk06/config.h
@@ -0,0 +1,16 @@
+
+#pragma once
+
+// TODO: some problem with GPIO macros
+//#include "config_common.h"
+
+#define PRODUCT           MK06 keyboard converter
+#define DESCRIPTION       MK06 keyboard converter
+
+#define MATRIX_ROWS       16
+#define MATRIX_COLS       16
+
+#define SD1_PORT PORT_A
+#define HOST_PORT PORT_A
+#define SD2_PORT PORT_X
+#define DLINK_PORT PORT_X
