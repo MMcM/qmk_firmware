9c4c7053464daa7e96e309c0ce881e1e2c989a2f
Mike McMahon <mmcm@comcast.net>
2022-05-28 20:11:23 -0700

    React to May 22 breaking changes * RESET -> QK_BOOT * octal not allowed in g_led_config

--- keyboards/walletburner/neuron/keymaps/mmcm/keymap.c
+++ keyboards/walletburner/neuron/keymaps/mmcm/keymap.c
@@ -9,7 +9,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_LCTL, KC_RALT, KC_SPC, KC_SPC, KC_SPC, KC_RALT, KC_RGUI),
 
 	[1] = LAYOUT(
-		KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, RESET, 
+		KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, QK_BOOT,
 		_______, _______, KC_UP,   _______, _______, _______, KC_LBRC, KC_RBRC, KC_SCLN, KC_QUOT, _______, 
 		_______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, KC_MINS, KC_EQL,  KC_BSLS, KC_SLSH, _______, 
 		_______, _______, _______, _______, _______, _______, _______)
