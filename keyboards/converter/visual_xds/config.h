f1bf2bbb975afcc7e1ed5b6b647f312733b938c7
Mike McMahon <mmcm@comcast.net>
2022-06-04 17:24:32 -0700

    converter/visual_xds

---
+++ keyboards/converter/visual_xds/config.h
@@ -0,0 +1,15 @@
+
+#pragma once
+
+#define PRODUCT           Visual XDS keyboard converter
+#define DESCRIPTION       Visual XDS keyboard converter
+
+#define MATRIX_ROWS       8
+#define MATRIX_COLS       16
+
+#define SERIAL_UART_BAUD 600
+#define SERIAL_UART_INIT_CUSTOM \
+    /* enable TX and RX with interrupt */ \
+    UCSR1B = _BV(TXEN1) | _BV(RXEN1) | _BV(RXCIE1); \
+    /* 8-bit data */ \
+    UCSR1C = _BV(UCSZ11) | _BV(UCSZ10);
--- users/mmcm/mmcm.sh
+++ users/mmcm/mmcm.sh
@@ -96,4 +96,5 @@ make converter/olivetti_m24 SILENT=true &&
 make converter/nec_pc_6601sr SILENT=true &&
 make converter/sperry_univac_uds_2000 SILENT=true &&
 make converter/wey_mk06 SILENT=true &&
+make converter/visual_xds SILENT=true &&
 true
