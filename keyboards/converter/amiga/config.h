041c636a92caf39b03225a0351bce39f1eaf836f
Mike McMahon <mmcm@comcast.net>
2022-09-03 12:25:03 -0700

    converter/amiga

---
+++ keyboards/converter/amiga/config.h
@@ -0,0 +1,9 @@
+
+#pragma once
+
+#include "config_common.h"
+
+#define PRODUCT           Amiga keyboard converter
+                          
+#define MATRIX_ROWS       7
+#define MATRIX_COLS       16
